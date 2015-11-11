fb634f368c452b2f971edf2b31e543cf3d56d2b1
JetMaShow <wongsamud@gmail.com>
2015-11-11 14:02:36 +0700

    com1

---
+++ test.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <script src="dist/sweetalert-dev.js"></script>
+  <link rel="stylesheet" href="dist/sweetalert.css">
+</head>
+<body>
+<script>
+swal("Hello Git!!!")
+</script>
+</body>
+ 
+ 
+ </html>
